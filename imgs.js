274c2d7f956e3948d0919113e9565610956e3499
matt-oconnell <mattoconnell408@gmail.com>
2017-03-27 10:38:06 -0400

    promisify, stream img read/write

--- imgs.js
+++ imgs.js
@@ -2,7 +2,6 @@ const fs        = require('fs');
 const https     = require('https');
 const axios     = require('axios');
 const path      = require('path');
-const Stream    = require('stream').Transform;
 const shuffle   = require('shuffle-array');
 const untildify = require('untildify');
 const args      = require('minimist')(process.argv.slice(2));
@@ -26,36 +25,49 @@ if (!args.key) {
 const endpoint  = 'http://www.behance.net/v2/projects';
 const key       = `client_id=${args.key}`;
 const searchQ   = encodeURIComponent(args.search || 'beeple everydays');
-const search    = `${endpoint}?${key}&q=${searchQ}`;
+const search    = `${endpoint}?${key}&q=${searchQ}&tags=illustration|digital+art|photography`;
 const targetDir = path.resolve(untildify(args.dir || '~/Desktop'));
 const limit     = args.limit || 4;
+let tries = 0;
 
-const download = (url, filename) => {
-  https.request(url, (response) => {
-    const data = new Stream();
-    response.on('data', chunk =>  data.push(chunk));
-    response.on('end', () => fs.writeFileSync(filename, data.read()));
-  }).end();
-}
+getImages();
 
-const getImages = async () => {
-  let count = 0;
+async function getImages() {
+  tries++;
+
   try {
     const { projects } = (await axios.get(search)).data;
     const { project }  = (await axios.get(`${endpoint}/${shuffle(projects)[0].id}?${key}`)).data;
-    const { modules }  = project;
-    modules.forEach((module) => {
-      if (count >= limit) {
-        return;
-      }
-      if (module.type == 'image') {
-        count++;
-        download(module.sizes.original, `${targetDir}/${count}.jpg`);
-      }
-    });
+    let { modules }  = project;
+    const downloadFuncs = [];
+
+    modules = modules.filter(module => module.type === 'image');
+
+    for (let i = 1; i <= limit; i++) {
+      const module = modules[i];
+      downloadFuncs.push(download(module.sizes.original, `${targetDir}/${i}.jpg`));
+    }
+
+    Promise.all(downloadFuncs)
+      .then(() => console.log('Behance imgs updated!'))
+      .catch(e => { throw new Error(e) });
+
   } catch(e) {
-    console.log(e);
+    if (tries < 3) {
+      getImages();
+    } else {
+      console.log('Error updating images', e);
+    }
   }
 }
 
-getImages();
+function download(url, filename) {
+  return new Promise((resolve, reject) => {
+    https.request(url, (response) => {
+      const writer = fs.createWriteStream(filename);
+      writer.on('finish', () => resolve());
+      writer.on('error', e => reject(e));
+      response.pipe(writer);
+    }).end();
+  });
+}
